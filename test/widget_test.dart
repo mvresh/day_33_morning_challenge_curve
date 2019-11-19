823a345d189de0c7e4d0fa25c0dab5d8eae27661
Ashish <ashishteddy2@gmail.com>
2019-11-19 10:31:28 +0530

    1.0.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,20 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:day_33_morning_challenge_curve/main.dart';
 
-
 void main() {
   Curve myCurve = MyCurve();
   print(myCurve.transform(0.5));
-  test("Testing curve value", (){
-    expect(myCurve.transformInternal(0.2), 0.0);
-    expect(myCurve.transformInternal(0.2), 0.0);
-    expect(myCurve.transformInternal(0.21), Curves.ease.transformInternal(0.01));});
-
-});
-    
+  test("Testing curve value", () {
+    expect(myCurve.transform(0.2), 0.0);
+    expect(myCurve.transform(0.2), 0.0);
+    expect(myCurve.transform(0.3), Curves.ease.transform(0.5));
+    expect(myCurve.transform(0.35), Curves.ease.transform(0.75));
+    expect(myCurve.transform(0.25), Curves.ease.transform(0.25));
+    expect(myCurve.transform(0.4), Curves.ease.transform(1));
+    expect(myCurve.transform(0.4), Curves.ease.transform(1));
+    expect(myCurve.transform(0.21), Curves.ease.transform(0.01));
+    expect(myCurve.transform(0.21), Curves.ease.transform(0.01));
+  });
 }
