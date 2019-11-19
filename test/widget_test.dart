d892eeefe96abd9dd265e5a25ead9cc60bc34452
Ashish <ashishteddy2@gmail.com>
2019-11-19 11:45:02 +0530

    done 1.2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,13 @@ void main() {
   Curve myCurve = MyCurve();
   print(myCurve.transform(0.5));
   test("Testing curve value", () {
-    expect(myCurve.transform(0.2), 0.0);
+    expect(myCurve.transform(0.101), 0.0);
     expect(myCurve.transform(0.2), 0.0);
     expect(myCurve.transform(0.3), Curves.ease.transform(0.5));
     expect(myCurve.transform(0.35), Curves.ease.transform(0.75));
     expect(myCurve.transform(0.25), Curves.ease.transform(0.25));
     expect(myCurve.transform(0.4), Curves.ease.transform(1));
+    expect(myCurve.transform(0.199), Curves.ease.transform(0));
     expect(myCurve.transform(0.45), Curves.decelerate.transform(0.75));
     expect(myCurve.transform(0.5), Curves.decelerate.transform(0.5));
     expect(myCurve.transform(0.55), Curves.decelerate.transform(0.25));
